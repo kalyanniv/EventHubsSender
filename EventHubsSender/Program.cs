f9f862ba8819980c81bdf744f4ecd17ff4a2ab67
kalyanniv <kalyanniv@gmail.com>
2016-08-21 11:50:53 +0100

    Comment from home removed

--- EventHubsSender/Program.cs
+++ EventHubsSender/Program.cs
@@ -13,7 +13,6 @@ namespace EventHubsSender
     class Program
     {
         // EventHub settings
-        // From home
         static string eventHubName = "kalyannivhub";
         static string connectionString = "Endpoint=sb://kalyannivhub-ns.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=eM0cyXRq8vIheb3KSYGjBBWGryutjucbd362Ne5hPpw=";
 
